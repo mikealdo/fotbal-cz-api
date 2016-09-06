48a6f25a561a121ecc37f9fef2cff987fdd37ba0
Michal Davídek <mdavidek1@gmail.com>
2016-09-06 08:38:25 +0200

    Cleanup and additional settings for bintray upload.

--- buildSrc/src/main/groovy/io/fourfinanceit/setup/ConsoleNotPresentException.java
+++
@@ -1,8 +0,0 @@
-package io.fourfinanceit.setup;
-
-import groovy.transform.Immutable;
-import groovy.transform.InheritConstructors;
-
-@InheritConstructors
-@Immutable
-public class ConsoleNotPresentException extends RuntimeException {}
